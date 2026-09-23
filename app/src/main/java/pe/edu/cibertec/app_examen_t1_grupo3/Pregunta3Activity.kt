6b53fc2b2fa4d34b7aaec46fd3067a7d0b086080
Pedro Emilio Lopez <elliotsand2@gmail.com>
2026-09-23 02:30:36 -0500

    pregunta 3 - Pedro Emilio Lopez

---
+++ app/src/main/java/pe/edu/cibertec/app_examen_t1_grupo3/Pregunta3Activity.kt
@@ -0,0 +1,20 @@
+package pe.edu.cibertec.app_examen_t1_grupo3
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Pregunta3Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_pregunta3)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
